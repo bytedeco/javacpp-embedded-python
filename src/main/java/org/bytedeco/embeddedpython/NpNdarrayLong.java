5be44b04b84cd207bd7c13afa67e55268b57cd35
Yu Kobayashi <yukoba@gmail.com>
2021-03-12 16:17:02 +0900

    Add toArray8d

--- src/main/java/org/bytedeco/embeddedpython/NpNdarrayLong.java
+++ src/main/java/org/bytedeco/embeddedpython/NpNdarrayLong.java
@@ -80,4 +80,109 @@ public class NpNdarrayLong extends NpNdarray {
         }
         return ary;
     }
+
+    public long[][][][] toArray4d() {
+        if (ndim() != 4) throw new RuntimeException("ndim != 4");
+
+        int[] strides = this.strides;
+        long[][][][] ary = new long[dimensions[0]][dimensions[1]][dimensions[2]][dimensions[3]];
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = 0; j < ary[i].length; j++) {
+                for (int k = 0; k < ary[i][j].length; k++) {
+                    for (int l = 0; l < ary[i][j][k].length; l++) {
+                        ary[i][j][k][l] = data[i * strides[0] + j * strides[1] + k * strides[2] + l * strides[3]];
+                    }
+                }
+            }
+        }
+        return ary;
+    }
+
+    public long[][][][][] toArray5d() {
+        if (ndim() != 5) throw new RuntimeException("ndim != 5");
+
+        int[] strides = this.strides;
+        long[][][][][] ary = new long[dimensions[0]][dimensions[1]][dimensions[2]][dimensions[3]][dimensions[4]];
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = 0; j < ary[i].length; j++) {
+                for (int k = 0; k < ary[i][j].length; k++) {
+                    for (int l = 0; l < ary[i][j][k].length; l++) {
+                        for (int m = 0; m < ary[i][j][k][l].length; m++) {
+                            ary[i][j][k][l][m] = data[i * strides[0] + j * strides[1] + k * strides[2] + l * strides[3] + m * strides[4]];
+                        }
+                    }
+                }
+            }
+        }
+        return ary;
+    }
+
+    public long[][][][][][] toArray6d() {
+        if (ndim() != 6) throw new RuntimeException("ndim != 6");
+
+        int[] strides = this.strides;
+        long[][][][][][] ary = new long[dimensions[0]][dimensions[1]][dimensions[2]][dimensions[3]][dimensions[4]][dimensions[5]];
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = 0; j < ary[i].length; j++) {
+                for (int k = 0; k < ary[i][j].length; k++) {
+                    for (int l = 0; l < ary[i][j][k].length; l++) {
+                        for (int m = 0; m < ary[i][j][k][l].length; m++) {
+                            for (int n = 0; n < ary[i][j][k][l][m].length; n++) {
+                                ary[i][j][k][l][m][n] = data[i * strides[0] + j * strides[1] + k * strides[2] + l * strides[3] + m * strides[4] + n * strides[5]];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ary;
+    }
+
+    public long[][][][][][][] toArray7d() {
+        if (ndim() != 7) throw new RuntimeException("ndim != 7");
+
+        int[] strides = this.strides;
+        long[][][][][][][] ary = new long[dimensions[0]][dimensions[1]][dimensions[2]][dimensions[3]][dimensions[4]][dimensions[5]][dimensions[6]];
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = 0; j < ary[i].length; j++) {
+                for (int k = 0; k < ary[i][j].length; k++) {
+                    for (int l = 0; l < ary[i][j][k].length; l++) {
+                        for (int m = 0; m < ary[i][j][k][l].length; m++) {
+                            for (int n = 0; n < ary[i][j][k][l][m].length; n++) {
+                                for (int o = 0; o < ary[i][j][k][l][m][n].length; o++) {
+                                    ary[i][j][k][l][m][n][o] = data[i * strides[0] + j * strides[1] + k * strides[2] + l * strides[3] + m * strides[4] + n * strides[5] + o * strides[6]];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ary;
+    }
+
+    public long[][][][][][][][] toArray8d() {
+        if (ndim() != 8) throw new RuntimeException("ndim != 8");
+
+        int[] strides = this.strides;
+        long[][][][][][][][] ary = new long[dimensions[0]][dimensions[1]][dimensions[2]][dimensions[3]][dimensions[4]][dimensions[5]][dimensions[6]][dimensions[7]];
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = 0; j < ary[i].length; j++) {
+                for (int k = 0; k < ary[i][j].length; k++) {
+                    for (int l = 0; l < ary[i][j][k].length; l++) {
+                        for (int m = 0; m < ary[i][j][k][l].length; m++) {
+                            for (int n = 0; n < ary[i][j][k][l][m].length; n++) {
+                                for (int o = 0; o < ary[i][j][k][l][m][n].length; o++) {
+                                    for (int p = 0; p < ary[i][j][k][l][m][n][o].length; p++) {
+                                        ary[i][j][k][l][m][n][o][p] = data[i * strides[0] + j * strides[1] + k * strides[2] + l * strides[3] + m * strides[4] + n * strides[5] + o * strides[6] + p * strides[7]];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ary;
+    }
 }
